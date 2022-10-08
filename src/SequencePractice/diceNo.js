bb1cdc258e1a90db75eab07c98247e588fa21a77
Surabhi951 <surabhibhagat95@gmail.com>
2022-10-08 11:37:25 +0530

    Sequence Practice Problems

---
+++ src/SequencePractice/diceNo.js
@@ -0,0 +1,2 @@
+let randomDiceNumber = Math.floor((Math.random() * 6) + 1);
+console.log("Dice number between(1-6): " + randomDiceNumber);
---
+++ src/SequencePractice/unitConversion.js
@@ -0,0 +1,9 @@
+let ft = 12;
+let result = 42 / 12;
+console.log("Feet is: " + result);
+let feet = 60 * 40;
+let meter = 0.3048;
+let result1 = feet * 0.3048;
+console.log("Meter is: " + result1);
+let acre = (25 * result1) / 4046.8;
+console.log("Area of 25 plots = "+ acre +" acres");
